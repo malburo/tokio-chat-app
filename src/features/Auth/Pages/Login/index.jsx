8c796b1cc67283ced232104f27b49fbca72b5104
malburo <tongquocbao0605@gmail.com>
2020-07-21 22:42:15 +0700

    Login-Form component

---
+++ src/features/Auth/Pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LoginForm from 'features/Auth/Components/LoginForm';
+
+LoginPage.propTypes = {
+
+};
+
+function LoginPage(props) {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
